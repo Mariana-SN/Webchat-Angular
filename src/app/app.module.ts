92199247930fd03038c4d2c5d3ebf2f9e45ce12a
Mariana-SN <mary.sutcliff13@gmail.com>
2023-07-10 18:26:10 -0300

    Criando componentes iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
