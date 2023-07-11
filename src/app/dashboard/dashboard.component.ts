3beec7c6f8d0e2a87e796b8d48a863a306175e4c
Raynara <raynaramartins.1999@gmail.com>
2023-07-11 01:40:38 -0300

    dashboard inicial

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  users: any[] = [];
+  searchKeyword: string = '';
+loggedInUser: any;
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.userService.getAllUsers().subscribe(
+      users => {
+        this.users = users;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  searchUsers() {
+    if (this.searchKeyword.trim() !== '') {
+      this.userService.searchUsers(this.searchKeyword).subscribe(
+        users => {
+          this.users = users;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    } else {
+      this.getUsers();
+    }
+  }
+
+  startChat(user: any) {
+    this.userService.startChat(user).subscribe(
+      response => {
+        console.log('Conversa iniciada com:', user);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private baseUrl = 'http://localhost:8080'; 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getLoggedInUser(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/user/login`);
+  }
+
+  getAllUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/user/all-users`);
+  }
+
+  searchUsers(keyword: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/user/all-users?keyword=${keyword}`);
+  }
+
+  startChat(user: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/message/send-message`, user);
+  }
 }
